e19548bd1d4c9ae528163c38e89f1ce72dd32018
CharlZ <charles.ceo2020@gmail.com>
2020-04-17 10:41:17 -0600

    imported inventory items

---
+++ src/components/Inventory.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { RootState } from '../store';
+import { removeItemFromInventory, addItemToInventory } from '../store/inventory/action';
+import { Item } from '../store/inventory/types';
+import { Grid } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+
+export interface IInventoryProps {
+  removeItemFromInventory: typeof removeItemFromInventory,
+  addItemToInventory: typeof addItemToInventory,
+  items: Item[]
+}
+
+export class Inventory extends React.Component<IInventoryProps>
+{
+  render ()
+  {
+    return (
+      <Grid>
+        {this.props.items.map( ( element, key ) => (<div>{element.name}</div>) )}
+      </Grid>
+    );
+  }
+}
+
+// Retrieve "items" from our "global" redux state.
+const mapStateToProps = ( state: RootState ) => {
+  return {
+    items: state.inventory.items
+  }
+}
+
+// Connect Redux and React using our values and "view!"
+export default connect(
+  mapStateToProps,
+  { addItemToInventory, removeItemFromInventory }
+)( Inventory );
